d7b4322f3a8cfdf5d864648039ae86660ac9c831
mohamedpsc <mohamedpsc@gmail.com>
2017-10-12 11:29:45 +0200

    Fixed Some bugs in sending and receiving functions

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import socket
 import threading
 
+
 class server():
         def __init__(self):
             self.running = True
 
-        # This Function Starts running the server
+        # Start running the server
         def run(self):
             # Create server socket
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,7 +22,8 @@ class server():
             while self.running:
                 data = input()
                 # Ending Chat
-                self.client_socket.send(data.encode())
+                if data != '':
+                    self.client_socket.send(data.encode())
                 if data == 'exit':
                     self.kill()
 
@@ -35,10 +37,11 @@ class server():
         def receive(self):
             while self.running:
                 data = self.client_socket.recv(1024).decode()
-                print("client: " + data)
                 if data in ['exit', '', None]:
                     # Chat Ended by Client
                     self.kill()
+                else:
+                    print("client: " + data)
 
 
 if __name__ == '__main__':
